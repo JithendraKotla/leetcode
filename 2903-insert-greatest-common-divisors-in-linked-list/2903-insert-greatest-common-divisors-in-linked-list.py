eaf76c78cb7ad51ab37a2ebb776dfcf69862779c
JithendraKotla <160527662+JithendraKotla@users.noreply.github.com>
2024-09-12 23:12:19 +0530

    Time: 99 ms (34.39%), Space: 21.1 MB (5.75%) - LeetHub

--- 2903-insert-greatest-common-divisors-in-linked-list/2903-insert-greatest-common-divisors-in-linked-list.py
+++ 2903-insert-greatest-common-divisors-in-linked-list/2903-insert-greatest-common-divisors-in-linked-list.py
@@ -12,33 +12,30 @@ class Solution(object):
         gcdi=[]
         temp=head
         arr=[]
-        
-        while(temp):
+        while temp:
             arr.append(temp.val)
             temp=temp.next
         if (len(arr)==1):
             return head
-        def gcd(a, b):
-            while b != 0:
-                a, b = b, a % b
+        def gcd(a,b):
+            while b!=0:
+                a,b=b,a%b
             return a
         for i in range(len(arr)-1):
             j=gcd(arr[i],arr[i+1])
             gcdi.append(j)
-        res = []
-       
-
-        res = []
-        for i in range(len(arr) - 1):
-            res.append(arr[i])     
-            res.append(gcdi[i]) 
+        res=[]
+        for i in range(len(arr)-1):
+            res.append(arr[i])
+            res.append(gcdi[i])
         res.append(arr[-1])
         dummy=ListNode(0)
         cur=dummy
         for i in res:
             cur.next=ListNode(i)
-            cur=cur.next
+            cur =cur.next
         return dummy.next
+        
        
         
 
