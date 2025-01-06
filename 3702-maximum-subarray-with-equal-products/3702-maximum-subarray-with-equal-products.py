ed7736d94790ce63dcd3d65f036e1786c2df0be9
JithendraKotla <160527662+JithendraKotla@users.noreply.github.com>
2025-01-06 07:52:12 +0530

    Time: 2502 ms (100%), Space: 12.5 MB (100%) - LeetHub

--- 3702-maximum-subarray-with-equal-products/3702-maximum-subarray-with-equal-products.py
+++ 3702-maximum-subarray-with-equal-products/3702-maximum-subarray-with-equal-products.py
@@ -1,12 +1,41 @@
-class Solution:
-    def maxLength(self, nums: List[int]) -> int:
+class Solution(object):
+    def maxLength(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        def gcd(a, b):
+            while b:
+                a, b = b, a % b
+            return a
+
+        def gcd_of_array(arr):
+            result = arr[0]
+            for num in arr[1:]:
+                result = gcd(result, num)
+            return result
+
+        def lcm(a, b):
+            return abs(a * b) // gcd(a, b)
+
+        def lcm_of_array(arr):
+            result = arr[0]
+            for num in arr[1:]:
+                result = lcm(result, num)
+            return result
+
+        def prod(arr):
+            pro = 1
+            for i in arr:
+                pro *= i
+            return pro
+
         res = 0
 
         for i in range(len(nums)):
-            for j in range(i + 1, len(nums)):
-                arr = nums[i:j+1]
-
-                if prod(arr) == lcm(*arr) * gcd(*arr):
+            for j in range(i, len(nums)):  
+                arr = nums[i:j + 1]
+                if prod(arr) == lcm_of_array(arr) * gcd_of_array(arr):
                     res = max(res, len(arr))
 
         return res
